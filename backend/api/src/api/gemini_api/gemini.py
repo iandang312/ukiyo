22caf2c9b20d11f55df779da2ce1822fbaf7ec8f
unknown <iandang312@gmail.com>
2026-02-03 18:43:07 -0600

    gemini setup

--- backend/api/src/api/gemini_api/gemini.py
+++ backend/api/src/api/gemini_api/gemini.py
@@ -1,6 +1,11 @@
 from google import genai
+from dotenv import load_dotenv
+import os
 
-client = genai.Client()
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 response = client.models.generate_content(
     model="gemini-3-flash-preview",
